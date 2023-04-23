58848c2aa7c8cf533c704e49727cd9ba2a0c093e
wanghui <w943168927@163.com>
2023-04-23 20:26:54 +0800

    初始化

---
+++ src/views/threescene/editor/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <canvas id="renderCanvas"></canvas>
+    </div>
+</template>
+<script setup>
+import { Playground } from "./Playground"
+
+window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+    let playground = new Playground(canvas);
+    playground.run();
+});
+</script>
