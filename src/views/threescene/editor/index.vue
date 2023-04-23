c38c9bfd29ab73b5a5e7f6c477c1ef3d409db69a
wanghui <w943168927@163.com>
2023-04-23 23:48:59 +0800

    --

--- src/views/threescene/editor/index.vue
+++ src/views/threescene/editor/index.vue
@@ -1,14 +1,33 @@
 <template>
-    <div>
-        <canvas id="renderCanvas"></canvas>
-    </div>
+  <div>
+    <a-button @click="creat" type="primary">Primary Button</a-button>
+    <canvas class="canvas" id="renderCanvas"></canvas>
+  </div>
 </template>
-<script setup>
-import { Playground } from "./Playground"
-
-window.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-    let playground = new Playground(canvas);
-    playground.run();
+<script lang="ts" setup>
+import { defineComponent, onMounted, ref, reactive } from 'vue';
+import { Playground } from './Playground';
+import * as BABYLON from 'babylonjs';
+let PG: any;
+onMounted(() => {
+  createScene();
 });
+function createScene() {
+  const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+  if (canvas) {
+    PG = new Playground(canvas);
+    PG.run();
+  }
+}
+function creat(){
+    console.log("1111");
+    PG.addSohere("qwe",{ diameter: 2, segments: 32 },PG.scene);
+}
 </script>
+<style scoped>
+.canvas {
+  width: 100%;
+  height: 100%;
+  touch-action: none;
+}
+</style>
